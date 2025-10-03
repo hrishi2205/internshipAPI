47110e14b46b3d5c4bc5622b6cd9248ee79fe1bd
hrishi2205 <hrishiyadav54@gmail.com>
2025-10-03 12:43:24 +0530

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <h1>Artworks from the Art Institute of Chicago</h1>
       <ArtworkTable />
     </div>
   );
